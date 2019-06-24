8f879d67f2d25a3e15f85914bc3e102413e42bed
Mark <33373431+borseno@users.noreply.github.com>
2019-06-24 23:20:33 +0300

    added api token

--- TimerBot/TimerBot/Startup.cs
+++ TimerBot/TimerBot/Startup.cs
@@ -33,7 +33,7 @@ namespace TimerBot
 
     internal static class Constants
     {
-        internal const string ApiToken = "{YOUR_BOT_API_TOKEN_WITHOUT_CURLY_BRACES}";
+        internal const string ApiToken = "51a6ef998e4a9f0ed5defdbe2e02f4d807214006";
         internal const string TimeSpanFormat = @"hh\:mm\:ss";
         internal const string StartCommand = @"/start";
         internal const string StartAnswer = @"Hello! You can set a timer to wait and get a notification as the timer runs out.
